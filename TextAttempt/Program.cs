ed29e09756c009d43a6b6859f4af5fc06bd898c1
Derek Gooding <djgooding@live.com>
2024-12-02 09:17:05 -0500

    Directly load to console

--- TextAttempt/Program.cs
+++ TextAttempt/Program.cs
@@ -8,7 +8,7 @@ public static class Program
     public static async Task Main()
     {
         WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault();
-        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<Layout.Console>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
