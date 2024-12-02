ed50fd57eebd06623c09bb7f29e01b567b2a3823
Derek Gooding <djgooding@live.com>
2024-12-02 14:54:30 -0500

    little more

---
+++ TextAttempt/Paragraphs.cs
@@ -0,0 +1,11 @@
+﻿using static ConsoleHero.ParagraphBuilder;
+
+namespace TextAttempt;
+
+public static class Paragraphs
+{
+    public static Paragraph Greeting =>
+    Line("Welcome traveller! What would you like to drink?").
+    GoTo(Menus.MainMenu).
+    DelayInSeconds(0);
+}
