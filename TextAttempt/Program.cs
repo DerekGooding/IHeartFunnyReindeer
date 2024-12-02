a930bab5735f8f66006aeb5ff8e0ec84d3d213a0
Derek Gooding <djgooding@live.com>
2024-12-02 09:21:33 -0500

    Removed bulk and simplified

--- TextAttempt/Program.cs
+++ TextAttempt/Program.cs
@@ -8,8 +8,8 @@ public static class Program
     public static async Task Main()
     {
         WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault();
-        builder.RootComponents.Add<Layout.Console>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.RootComponents.Add<Console>("#app");
+        //builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
